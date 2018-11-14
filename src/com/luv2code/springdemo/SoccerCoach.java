e8cc07038a732bce7e63f6b0039a6412e8094f93
lass5643 <lass5643@stud.kea.dk>
2018-11-14 17:02:41 +0100

    Qualifiers for Dependency Injection
    Making use of @Qualifier annotation

--- src/com/luv2code/springdemo/SoccerCoach.java
+++ src/com/luv2code/springdemo/SoccerCoach.java
@@ -1,11 +1,14 @@
 package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SoccerCoach implements Coach {
 
+	@Autowired
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 	
 	// define a default constructor
@@ -13,12 +16,14 @@ public class SoccerCoach implements Coach {
 		System.out.println(">> SoccerCoach: inside default constructor");
 	}
 	
+	/*
 	// define a setter method
 	@Autowired
 	public void setFortuneService(FortuneService theFortuneService) {
 		System.out.println(">> SoccersCoach: inside setFortuneService() method");
 		fortuneService = theFortuneService;
 	}
+	*/
 	
 	/*
 	public SoccerCoach(FortuneService fortuneService) {
